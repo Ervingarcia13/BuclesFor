16e5dcc6f791e1cfaec69179ca0b8da091740c56
Ervingarcia13 <ervingarcia13@hotmail.com>
2023-01-08 23:06:03 -0500

    Ciclo  for  , haciendo un ejemplo

---
+++ src/main/java/co/com/BuclesFor/BuclesFor.java
@@ -0,0 +1,6 @@
+package co.com.BuclesFor;
+
+public class BuclesFor {
+
+
+}
---
+++ src/main/java/co/com/BuclesFor/Main.java
@@ -0,0 +1,17 @@
+package co.com.BuclesFor;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int valores[] = { 10,20,30,40,50};
+
+        for (int i=0; i< valores.length; i++){
+
+            System.out.println( "La respuesta es " +valores[i] );
+
+        }
+    }
+
+
+
+}
